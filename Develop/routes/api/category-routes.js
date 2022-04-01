9f5b20dd8b0ff275558e2fac67692738da370df1
Catherine McNeel <catherinemcneel@icloud.com>
2022-04-01 13:30:10 -0400

    added setup and category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,18 +3,63 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+    try {
+      const categoriesData = await Category.findAll({
+        // JOIN with products     
+        include: [ {model: Product} ]
+      })
+      res.status(200).json(categoriesData);
+  
+    } catch (err) {
+      //console log error if status = 500
+      res.status(500).json(err);
+    }
+  });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [ {model: Product} ]
+  
+  })
+  .then(dbE_commerce => {
+    if (!dbE_commerce) {
+      res.status(404).json({ message: 'No product found with this id'});
+      return;
+    }
+    res.json(dbE_commerce);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then(dbE_commerce => {
+      req.session.save(() => {
+        req.session.category_name = dbE_commerce.category_name;
+
+        res.json(dbE_commerce);
+      });
+    })
+      .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +68,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  then(dbE_commerce => {
+    if (!dbE_commerce) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbE_commerce)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const dotenv = require('dotenv')
+const mysql = require('mysql2');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+require('dotenv').config()
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// sequelize.sync creates new tables according to the schema specified in the model.
+// this schema is found in models/index.js
+// new tables are created according to the schema defined in each model file
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App now running on port ${PORT}`));
 });
